1e5a4093289d96c13a14236a06aaacc4a35da3b4
webstormcomputers <aaron@webstormcomputers.com>
2018-04-04 17:44:53 -0500

    Working on Next button

--- app/src/main/java/com/webstormcomputers/runes2/MainActivity.java
+++ app/src/main/java/com/webstormcomputers/runes2/MainActivity.java
@@ -9,41 +9,39 @@ TODO
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import static com.webstormcomputers.runes2.R.id.WrName;
 
+
+
+
+
 public class MainActivity extends AppCompatActivity {
- @Override
+    int indexId = 0;
+    TextView wordruneId;
+    TextView Wrname;
+    TextView Reference;
+    TextView Script;
+    TextView chstreq;
+    TextView ssreq;
+    TextView eff;
+    TextView dur;
+    TextView chincr;
+    TextView ssincr;
+    Button Next;
+    Button Previous;
+    wordune[] runes = runeslist.create_wordrunes();
+
+    @Override
  protected void onCreate(Bundle savedInstanceState) {
   super.onCreate(savedInstanceState);
   setContentView(R.layout.activity_main);
-  wordune[] runes = runeslist.create_wordrunes();
-/**wordune[] runes = new wordune[2];
-     runes[0] = new wordune();
-     runes[0].IdWordRune = 1;
-     runes[0].WrName = "Night Light Wordrune";
-     runes[0].scripture = "Thy Word is a lamp unto my feet and a light unto my path";
-     runes[0].reference = "Psalms 119:105";
-     runes[0].ChStReq = "JO 4";
-     runes[0].SsReq = "DL1";
-     runes[0].effect = "When a LightRaider says the Night Light WordRune, an area 20 feet ahead of the\n" +
-             "LightRaider is lit up. The light moves with the player. The Night Light lasts until it is\n" +
-             "commanded to leave.";
-     runes[0].duration = "May be used as often as needed";
-     runes[0].ChStIncr = "JO+1mu";
-     runes[0].SsIncr = "SS+1mu";
-*/
-  TextView wordruneId;
-  TextView Wrname;
-  TextView Reference;
-  TextView Script;
-  TextView chstreq;
-  TextView ssreq;
-  TextView eff;
-  TextView dur;
-  TextView chincr;
-  TextView ssincr;
+
+
+
 
   wordruneId = (TextView)findViewById(R.id.wordruneId);
   Wrname = (TextView) findViewById(WrName);
@@ -55,19 +53,29 @@ public class MainActivity extends AppCompatActivity {
   dur = (TextView) findViewById(R.id.Duration);
   chincr = (TextView) findViewById(R.id.ChStIncr);
   ssincr = (TextView) findViewById(R.id.SSIncr);
+  Next = (Button) findViewById(R.id.Next);
+  Previous = (Button) findViewById(R.id.Prev);
 
-
-
-  wordruneId.setText(Integer.toString(runes[0].IdWordRune));
-  Wrname.setText(runes[0].WrName);
-  Reference.setText(runes[0].reference);
-  Script.setText(runes[0].scripture);
-  chstreq.setText(runes[0].ChStReq);
-  ssreq.setText(runes[0].SsReq);
-  eff.setText(runes[0].effect);
-  dur.setText(runes[0].duration);
-  chincr.setText(runes[0].ChStIncr);
-  ssincr.setText(runes[0].SsIncr);
 
  }
+    public void nextOnclick(View view){
+        while (indexId < runes.length) {
+
+
+        wordruneId.setText(Integer.toString(runes[indexId].IdWordRune));
+        Wrname.setText(runes[indexId].WrName);
+        Reference.setText(runes[indexId].reference);
+        Script.setText(runes[indexId].scripture);
+        chstreq.setText(runes[indexId].ChStReq);
+        ssreq.setText(runes[indexId].SsReq);
+        eff.setText(runes[indexId].effect);
+        dur.setText(runes[indexId].duration);
+        chincr.setText(runes[indexId].ChStIncr);
+        ssincr.setText(runes[indexId].SsIncr);
+        indexId++;
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/webstormcomputers/runes2/runeslist.java
+++ app/src/main/java/com/webstormcomputers/runes2/runeslist.java
@@ -6,26 +6,38 @@ package com.webstormcomputers.runes2;
 
 public class runeslist {
 
- public static wordune[] create_wordrunes() {
-  wordune[] runelist = new wordune[2];
-  //runelist[0].IdWordRune = 0;
-  //runelist[0].WrName = "";
-  runelist[0] = new wordune();
-  runelist[0].IdWordRune = 1;
-  runelist[0].WrName = "Night Light Wordrune";
-  runelist[0].scripture = "Thy Word is a lamp unto my feet and a light unto my path";
-  runelist[0].reference = "Psalms 119:105";
-  runelist[0].ChStReq = "JO 4";
-  runelist[0].SsReq = "DL1";
-  runelist[0].effect = "When a LightRaider says the Night Light WordRune, an area 20 feet ahead of the\n" +
-          "LightRaider is lit up. The light moves with the player. The Night Light lasts until it is\n" +
-          "commanded to leave.";
-  runelist[0].duration = "May be used as often as needed";
-  runelist[0].ChStIncr = "JO+1mu";
-  runelist[0].SsIncr = "SS+1mu";
+    public static wordune[] create_wordrunes() {
+        wordune[] runelist = new wordune[2];
 
- return runelist;
- }
+        runelist[0] = new wordune();
+        runelist[0].IdWordRune = 1;
+        runelist[0].WrName = "Not Me WordRune";
+        runelist[0].scripture = "Do not be anxious about anything, but in everything, by prayer and petition,\n" +
+                "with thanksgiving, present your requests to God.";
+        runelist[0].reference = "Phillipians 4:6";
+        runelist[0].ChStReq = "PE 4";
+        runelist[0].SsReq = "DL2";
+        runelist[0].effect = "Anyone who can say the Not Me WordRune perfectly can reduce the muk sin enchantment\n" +
+                "by 2 difficulty levels";
+        runelist[0].duration = "May be used only once per day.";
+        runelist[0].ChStIncr = "PE+1mu";
+        runelist[0].SsIncr = "SS+2mu";
+
+        runelist[1] = new wordune();
+        runelist[1].IdWordRune = 2;
+        runelist[1].WrName = "Burden Free WordRune";
+        runelist[1].scripture = "Come to me, all you who are weary and burdened, and I will give you rest";
+        runelist[1].reference = "Matthew 11:28";
+        runelist[1].ChStReq = "JO 4";
+        runelist[1].SsReq = "DL1";
+        runelist[1].effect ="Anyone who can say the Burden Free WordRune perfectly can reduce the grim sin";
+        runelist[1].duration = "May be used once per day";
+        runelist[1].ChStIncr = "JO+1mu";
+        runelist[1].SsIncr = "SS+1mu";
+
+
+        return runelist;
+    }
 
 
 }
