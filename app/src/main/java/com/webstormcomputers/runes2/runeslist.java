4fbc5c40c6e7c076151fb4965bb06c952c9d1afa
Aaron Mitcham <aaron@webstormcomputers.com>
2018-04-12 08:10:37 -0500

    More layout tweaking And adding more wordrunes.

--- app/src/main/java/com/webstormcomputers/runes2/runeslist.java
+++ app/src/main/java/com/webstormcomputers/runes2/runeslist.java
@@ -7,7 +7,7 @@ package com.webstormcomputers.runes2;
 public class runeslist {
 
     public static wordune[] create_wordrunes() {
-        wordune[] runelist = new wordune[7];
+        wordune[] runelist = new wordune[10];
 
         runelist[0] = new wordune();
         runelist[0].IdWordRune = 1;
@@ -30,7 +30,7 @@ public class runeslist {
         runelist[1].reference = "Matthew 11:28";
         runelist[1].ChStReq = "JO 4";
         runelist[1].SsReq = "DL1";
-        runelist[1].effect ="Anyone who can say the Burden Free WordRune perfectly can reduce the grim sin enchantment";
+        runelist[1].effect ="Anyone who can say the Burden Free WordRune perfectly can reduce the grim sin enchantment by 2 difficulty levels";
         runelist[1].duration = "May be used once per day";
         runelist[1].ChStIncr = "JO+1mu";
         runelist[1].SsIncr = "SS+1mu";
@@ -38,9 +38,7 @@ public class runeslist {
         runelist[2] = new wordune();
         runelist[2].IdWordRune = 3;
         runelist[2].WrName = "Father's Desires WordRune";
-        runelist[2].scripture = "You belong to your father, the devil, and you want to carry out your father's\n" +
-                "desire. He was a murderer from the beginning, not holding to the truth, for" +
-                "there is no truth in him.";
+        runelist[2].scripture = "You belong to your father, the devil, and you want to carry out your father's desire. He was a murderer from the beginning, not holding to the truth, for there is no truth in him.";
         runelist[2].reference = "John 8:44a";
         runelist[2].ChStReq = "PA 4";
         runelist[2].SsReq = "DL3";
@@ -53,7 +51,7 @@ public class runeslist {
         runelist[3] = new wordune();
         runelist[3].IdWordRune = 4;
         runelist[3].WrName = "Chosen People WordRune";
-        runelist[3].scripture = "Therefore, as God's chosen people, holy and dearly loved, clothe yourselve with compassion, kindness, humility, gentleness and patience.";
+        runelist[3].scripture = "Therefore, as God's chosen people, holy and dearly loved, clothe yourselves with compassion, kindness, humility, gentleness and patience.";
         runelist[3].reference = "Colossians 3:12";
         runelist[3].ChStReq = "GE 4";
         runelist[3].SsReq = "DL1";
@@ -98,7 +96,41 @@ public class runeslist {
         runelist[6].ChStIncr = "GE+1mu";
         runelist[6].SsIncr = "SS+1mu";
 
+        runelist[7] = new wordune();
+        runelist[7].IdWordRune = 8;
+        runelist[7].WrName = "Richest Promise WordRune";
+        runelist[7].scripture = "Keep your lives free from the love of money and be content with what you have, because God has said, \"Never will I leave you; never will I forsake you.\"";
+        runelist[7].reference = "Hebrews 13:5";
+        runelist[7].ChStReq = "JO 4";
+        runelist[7].SsReq = "DL3";
+        runelist[7].effect ="Anyone who can say the Richest Promise WordRune perfectly can reduce the water troll sin enchantment by 2 difficulty levels.";
+        runelist[7].duration = "May be used once per day";
+        runelist[7].ChStIncr = "JO+2mu";
+        runelist[7].SsIncr = "SS+3mu";
 
+        runelist[8] = new wordune();
+        runelist[8].IdWordRune = 9;
+        runelist[8].WrName = "Hope Way WordRune";
+        runelist[8].scripture = "Why are you downcast, O my soul? Why so disturbed within me? Put your hope in God,. . .";
+        runelist[8].reference = "Psalm 42:5a";
+        runelist[8].ChStReq = "HO 4";
+        runelist[8].SsReq = "DL1";
+        runelist[8].effect ="Anyone who can say the Hope Way WordRune perfectly can reduce the skeleton sin enchantment by 2 difficulty levels.";
+        runelist[8].duration = "May be used once per day";
+        runelist[8].ChStIncr = "JO+1mu";
+        runelist[8].SsIncr = "SS+1mu";
+
+        runelist[9] = new wordune();
+        runelist[9].IdWordRune = 10;
+        runelist[9].WrName = "Father's Will WordRune";
+        runelist[9].scripture = "Not everyone who says to me, \"Lord, Lord,\" will enter the kingdom of heaven, but only he who does the will of my Father who is in heaven.";
+        runelist[9].reference = "Matthew 7:21";
+        runelist[9].ChStReq = "FA 4";
+        runelist[9].SsReq = "DL2";
+        runelist[9].effect ="Anyone who can say the Father's Will WordRune perfectly can reduce the selfoe sin enchantment by 2 difficulty levels.";
+        runelist[9].duration = "May be used once per day";
+        runelist[9].ChStIncr = "FA+1mu";
+        runelist[9].SsIncr = "SS+2mu";
 
         return runelist;
     }
